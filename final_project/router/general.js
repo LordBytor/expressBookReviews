4fcaa73a6f3951e76bd9519788a80e6dd069eaca
LordBytor <lordbytor@gmail.com>
2025-07-21 20:43:59 -0400

    updated general.js.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,25 +13,38 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented "});
+  res.send(JSON.stringify({books}, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = parseInt(req.params.isbn);
+   // let filtered_books = books.filter((book) => book.isbn === isbn);
+  //return res.status(300).json({message: "Yet to be implemented" + JSON.stringify(books[isbn])});
+  res.send(books[isbn]);
+  //res.send(JSON.stringify({books[isbn]}, null, 4));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const bookList = Object.values(books);
+  let filtered_books_by_author = bookList.filter((book) => book.author === author);
+  res.send(filtered_books_by_author);
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const bookList = Object.values(books);
+  let filtered_books_by_title = bookList.filter((book) => book.title === title);
+  res.send(filtered_books_by_title);
 });
 
 //  Get book review
