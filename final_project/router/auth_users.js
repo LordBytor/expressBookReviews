d7e77be345c52b55f15b840103eb9ac09dfd5867
LordBytor <lordbytor@gmail.com>
2025-07-26 20:35:11 -0400

    updated auth_users

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,23 +6,62 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+    let filetered_users = users.filter((user) => user.username == username);
+    if(filetered_users.length > 0)
+    {
+        return true;
+    }
+    return false;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    let filetered_users = users.filter((user) => user.username == username);
+
+    if (filetered_users.length > 0 && filetered_users[0].password === password)
+    {
+        return true;
+    }
+    return false;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+    const user = req.body.user;
+    if (!user) {
+        return res.status(404).json({ message: "Error, no body" });
+    }
+     if (!user.username || !isValid(user.username)) {
+        return res.status(404).json({ message: "Invalid user" });
+    }   
+
+    if(!user.password || !authenticatedUser(user.username, user.password))
+    {
+        return res.status(404).json({ message: "Invalid password for " + user.username });
+    }
+
+    // Generate JWT access token
+    let accessToken = jwt.sign({
+        data: user
+    }, 'access', { expiresIn: 60 * 60 });
+
+    // Store access token in session
+    req.session.authorization = {
+        accessToken
+    }
+    return res.status(200).send("User successfully logged in");
+
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const isbn = parseInt(req.params.isbn);
+  //let allReviews = books[isbn].reviews;
+  //let filteredReviews = allReviews.filter((user) => user.username == username);
+  //res.send(books[isbn].reviews);
+   return res.status(300).json({message: req.session.authorization.data + "wants a book"});
 });
 
 module.exports.authenticated = regd_users;
